81934ccdd8ae9cca3f35d22a9c93069f247de2a7
atomoa <tomo92588481129@icloud.com>
2024-01-12 12:38:14 +0900

    db接続

--- src/index.php
+++ src/index.php
@@ -24,11 +24,11 @@
 <hr>
 <?php 
 echo '<table>';
-echo '<tr><th>選手名</th><th>ポジション</th><th>価格</th></tr>';
+echo '<tr><th>選手名</th><th>チーム</th><th>ポジション</th></tr>';
 $pdo =new PDO($connect,USER,PASS);
 
 if(isset($_POST['keyword'])){
-    $sql=$pdo->prepare('select * from product where name like ?');
+    $sql=$pdo->prepare('select * from baseball where name like ?');
     $sql->execute(['%'.$_POST['keyword'].'%']);
 }else{
     $sql=$pdo->query('select * from baseball');
@@ -36,11 +36,9 @@ if(isset($_POST['keyword'])){
 foreach($sql as $row){
     $id=$row['id'];
     echo '<tr>';
-    echo '<td>',$id,'</td>';
-    echo '<td>';
-    echo '<a href="detail.php?id=', $id, '">', $row['name'],'</a>';
-    echo '</td>';
-    echo '<td>', $row['price'],'</td>';
+    echo '<td>',$row['name'],'</td>';
+    echo '<td>',$row['team'],'</td>';
+    echo '<td>', $row['position'],'</td>';
     echo '</tr>';
 }
 echo '</table>';
