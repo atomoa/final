642f5a4d80f9d96b308e47139ef8198a8223e547
atomoa <tomo92588481129@icloud.com>
2024-01-12 14:42:48 +0900

    ふぉーむ

--- src/index.php
+++ src/index.php
@@ -16,9 +16,30 @@
 <h1>自分好みのオーダーを組もう！</h1>
 <p>※選手情報は2023シーズン終了時のものになります</p>
 <p>※選手は各チームの支配下選手のみとなります</p>
-<form action="index.php" method="post">
-    選手検索
-    <input type="text" name="keyword">
+<form method="get" action="index.php">
+    <input type="text" name="keyword" placeholder="選手名を入力">
+    <select name="team">
+        <option value="">すべてのチーム</option>
+        <option value="tigers">阪神タイガース</option>
+        <option value="baystars">横浜DeNAベイスターズ</option>
+        <option value="carp">広島東洋カープ</option>
+        <option value="gians">巨人</option>
+        <option value="swallows">東京ヤクルトスワローズ</option>
+        <option value="dragons">中日ドラゴンズ</option>
+        <option value="buffaloes">オリックス・バファローズ</option>
+        <option value="marines">千葉ロッテマリーンズ</option>
+        <option value="hawks">福岡ソフトバンクホークス</option>
+        <option value="eagles">東北楽天ゴールデンイーグルス</option>
+        <option value="lions">埼玉西武ライオンズ</option>
+        <option value="fighters">北海道日本ハムファイターズ</option>
+    </select>
+    <select name="position">
+        <option value="">すべてのポジション</option>
+        <option value="catcher">捕手</option>
+        <option value="infielder">内野手</option>
+        <option value="outfielder">外野手</option>
+        <option value="pitcher">投手</option>
+    </select>
     <input type="submit" value="検索">
 </form>
 <hr>
